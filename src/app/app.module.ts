901a81f4d04d9903cf72c64df4416089fe0f114f
Maksim Frese <maksimfrese@Air-von-Maksim.fritz.box>
2022-04-25 21:33:59 +0200

    use new names of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,19 @@ import { SwiperModule } from 'swiper/angular';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { TabsComponent } from './tabs/tabs.component';
-import { DrumsComponent } from './drums/drums.component';
-import { DrumItemComponent } from './drums/drum-item/drum-item.component';
+import { MachinesComponent } from './machines/machines.component';
+import { MachineItemComponent } from './machines/machine-item/machine-item.component';
 import { HelpComponent } from './help/help.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DrumsComponent,
+    MachinesComponent,
     MenuComponent,
     TabsComponent,
-    DrumsComponent,
+    MachinesComponent,
     HelpComponent,
-    DrumItemComponent
+    MachineItemComponent
   ],
   imports: [
     BrowserModule,
