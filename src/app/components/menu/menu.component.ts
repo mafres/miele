a6e428f0bb559f87d5b33484e8f2a53a64b1b00d
Maksim Frese <maksimfrese@Air-von-Maksim.fritz.box>
2022-04-27 00:54:59 +0200

    add demo for store communication to menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {select, Store} from "@ngrx/store";
+import * as rootReducer from "../../reducers";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
+import {selectedProgramInterface} from "../../models/selected-program.interface";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
+  selectedProgramVisible: boolean = false;
+  activeProgram$: Observable<{ id: number, name: string }>;
 
-  ngOnInit(): void {
+  constructor(
+    private _store: Store<rootReducer.State>,
+  ) {
+    this.activeProgram$ = this._store.pipe(
+      select(state => state.selectedProgram)
+    );
   }
 
+  toggleSelectedProgram() {
+    this.selectedProgramVisible = !this.selectedProgramVisible
+  }
 }
