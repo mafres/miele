27790806fc123ee0499c3e6fc354c614e51479fc
Maksim Frese <maksimfrese@Air-von-Maksim.fritz.box>
2022-04-27 14:06:16 +0200

    move menu store demonstration to only console output

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,29 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import {select, Store} from "@ngrx/store";
-import * as rootReducer from "../../reducers";
-import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
-import {selectedProgramInterface} from "../../models/selected-program.interface";
+import { Component } from '@angular/core';
+import {
+  select,
+  Store
+} from '@ngrx/store';
+import * as rootReducer from '../../reducers';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent {
-
-  selectedProgramVisible: boolean = false;
-  activeProgram$: Observable<{ id: number, name: string }>;
 
   constructor(
     private _store: Store<rootReducer.State>,
   ) {
-    this.activeProgram$ = this._store.pipe(
+    this._store.pipe(
       select(state => state.selectedProgram)
-    );
+    ).subscribe(value => {
+      console.log(value);
+    });
   }
 
-  toggleSelectedProgram() {
-    this.selectedProgramVisible = !this.selectedProgramVisible
-  }
 }
